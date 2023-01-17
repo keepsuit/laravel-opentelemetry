68a0415ceaabeb4302ba494ef24362840cf79ea2
Fabio Capucci <fabio.cappuc@gmail.com>
2023-01-17 17:42:01 +0100

    redis watcher

---
+++ tests/Watchers/RedisWatcherTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Arr;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\SDK\Common\Time\ClockFactory;
+use OpenTelemetry\SDK\Trace\ImmutableSpan;
+
+it('can watch a redis call', function () {
+    \Illuminate\Support\Facades\Redis::connection('default')->get('test');
+
+    flushSpans();
+
+    $span = Arr::last(getRecordedSpans());
+    assert($span instanceof ImmutableSpan);
+
+    expect($span)
+        ->getName()->toBe('redis default get')
+        ->getKind()->toBe(SpanKind::KIND_CLIENT)
+        ->getAttributes()->toArray()->toMatchArray([
+            'db.system' => 'redis',
+            'db.statement' => 'get test',
+            'net.peer.name' => '127.0.0.1',
+        ])
+        ->hasEnded()->toBeTrue()
+        ->getEndEpochNanos()->toBeLessThan(ClockFactory::getDefault()->now());
+
+    expect($span->getEndEpochNanos() - $span->getStartEpochNanos())
+        ->toBeGreaterThan(0);
+});
