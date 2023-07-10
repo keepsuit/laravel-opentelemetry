375f270f34ab5f3b136aeee30c33510729bddfd3
Fabio Capucci <fabio.cappuc@gmail.com>
2023-07-10 10:14:20 +0200

    support latest sdk version

--- src/Instrumentation/EventInstrumentation.php
+++ src/Instrumentation/EventInstrumentation.php
@@ -53,49 +53,4 @@ class EventInstrumentation implements Instrumentation
     {
         return in_array($event, static::$ignoredEvents);
     }
-
-//    public function recordCacheHit(CacheHit $event): void
-//    {
-//        $this->addEvent('cache hit', [
-//            'key' => $event->key,
-//            'tags' => json_encode($event->tags),
-//        ]);
-//    }
-//
-//    public function recordCacheMiss(CacheMissed $event): void
-//    {
-//        $this->addEvent('cache miss', [
-//            'key' => $event->key,
-//            'tags' => json_encode($event->tags),
-//        ]);
-//    }
-//
-//    /** @psalm-suppress UndefinedPropertyFetch */
-//    public function recordCacheSet(KeyWritten $event): void
-//    {
-//        $ttl = property_exists($event, 'minutes')
-//            ? $event->minutes * 60
-//            : $event->seconds;
-//
-//        $this->addEvent('cache set', [
-//            'key' => $event->key,
-//            'tags' => json_encode($event->tags),
-//            'expires_at' => $ttl > 0 ? now()->addSeconds($ttl)->getTimestamp() : 'never',
-//            'expires_in_seconds' => $ttl > 0 ? $ttl : 'never',
-//            'expires_in_human' => $ttl > 0 ? now()->addSeconds($ttl)->diffForHumans() : 'never',
-//        ]);
-//    }
-//
-//    public function recordCacheForget(KeyForgotten $event): void
-//    {
-//        $this->addEvent('cache forget', [
-//            'key' => $event->key,
-//            'tags' => json_encode($event->tags),
-//        ]);
-//    }
-//
-//    private function addEvent(string $name, iterable $attributes = []): void
-//    {
-//        Tracer::activeSpan()->addEvent($name, $attributes);
-//    }
 }
