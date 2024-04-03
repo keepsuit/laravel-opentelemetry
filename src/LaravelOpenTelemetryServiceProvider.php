6a854f33e77e3b8dafccd41b16817c46e14df1ac
Fabio Capucci <fabio.cappuc@gmail.com>
2024-04-03 15:06:49 +0200

    wip: logs support

--- src/LaravelOpenTelemetryServiceProvider.php
+++ src/LaravelOpenTelemetryServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Keepsuit\LaravelOpenTelemetry;
 
 use Composer\InstalledVersions;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Env;
 use Illuminate\Support\Str;
 use Keepsuit\LaravelOpenTelemetry\Instrumentation\Instrumentation;
@@ -15,6 +16,7 @@ use OpenTelemetry\API\Trace\TracerInterface;
 use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
 use OpenTelemetry\Contrib\Grpc\GrpcTransportFactory;
 use OpenTelemetry\Contrib\Otlp\HttpEndpointResolver;
+use OpenTelemetry\Contrib\Otlp\LogsExporter;
 use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
 use OpenTelemetry\Contrib\Otlp\OtlpUtil;
 use OpenTelemetry\Contrib\Otlp\SpanExporter as OtlpSpanExporter;
@@ -22,7 +24,15 @@ use OpenTelemetry\Contrib\Zipkin\Exporter as ZipkinExporter;
 use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Common\Configuration\Variables as OTELVariables;
 use OpenTelemetry\SDK\Common\Export\Http\PsrTransportFactory;
+use OpenTelemetry\SDK\Common\Export\TransportInterface;
 use OpenTelemetry\SDK\Common\Time\ClockFactory;
+use OpenTelemetry\SDK\Logs\Exporter\ConsoleExporter;
+use OpenTelemetry\SDK\Logs\Exporter\ConsoleExporterFactory as LogsConsoleExporterFactory;
+use OpenTelemetry\SDK\Logs\Exporter\InMemoryExporter;
+use OpenTelemetry\SDK\Logs\Exporter\InMemoryExporterFactory as LogsInMemoryExporterFactory;
+use OpenTelemetry\SDK\Logs\LoggerProvider;
+use OpenTelemetry\SDK\Logs\LogRecordExporterInterface;
+use OpenTelemetry\SDK\Logs\Processor\BatchLogRecordProcessor;
 use OpenTelemetry\SDK\Resource\ResourceInfo;
 use OpenTelemetry\SDK\Resource\ResourceInfoFactory;
 use OpenTelemetry\SDK\Sdk;
@@ -38,10 +48,10 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravelOpenTelemetryServiceProvider extends PackageServiceProvider
 {
-    public function bootingPackage(): void
+    public function packageBooted(): void
     {
         $this->configureEnvironmentVariables();
-        $this->initTracer();
+        $this->init();
         $this->registerInstrumentation();
     }
 
@@ -52,7 +62,7 @@ class LaravelOpenTelemetryServiceProvider extends PackageServiceProvider
             ->hasConfigFile();
     }
 
-    protected function initTracer(): void
+    protected function init(): void
     {
         ClockFactory::setDefault(new CarbonClock());
 
@@ -62,37 +72,44 @@ class LaravelOpenTelemetryServiceProvider extends PackageServiceProvider
             ]))
         );
 
-        $spanExporter = match (config('opentelemetry.exporter')) {
-            'zipkin' => new ZipkinExporter(
-                PsrTransportFactory::discover()->create(
-                    Str::of(config('opentelemetry.exporters.zipkin.endpoint'))->rtrim('/')->append('/api/v2/spans')->toString(),
-                    'application/json'
-                ),
-            ),
-            'otlp' => $this->buildOtlpExporter(),
-            'console' => (new ConsoleSpanExporterFactory())->create(),
-            default => (new InMemorySpanExporterFactory())->create(),
-        };
+        $propagator = PropagatorBuilder::new()->build(config('opentelemetry.propagators'));
 
-        $spanProcessor = (new BatchSpanProcessorBuilder($spanExporter))
-            ->build();
+        /**
+         * Traces
+         */
+        $spanExporter = $this->buildSpanExporter();
+        $spanProcessor = (new BatchSpanProcessorBuilder($spanExporter))->build();
 
+        $samplerConfig = config('opentelemetry.traces.sampler', []);
         $sampler = SamplerBuilder::new()->build(
-            config('opentelemetry.sampler.type'),
-            config('opentelemetry.sampler.parent'),
-            config('opentelemetry.sampler.args', [])
+            $samplerConfig['type'] ?? 'always_on',
+            $samplerConfig['parent'] ?? true,
+            $samplerConfig['args'] ?? []
         );
 
         $tracerProvider = TracerProvider::builder()
-            ->addSpanProcessor($spanProcessor)
             ->setResource($resource)
+            ->addSpanProcessor($spanProcessor)
             ->setSampler($sampler)
             ->build();
 
-        $propagator = PropagatorBuilder::new()->build(config('opentelemetry.propagators'));
+        /**
+         * Logs
+         */
+        $logExporter = $this->buildLogsExporter();
+        $logProcessor = new BatchLogRecordProcessor(
+            exporter: $logExporter,
+            clock: ClockFactory::getDefault()
+        );
+
+        $loggerProvider = LoggerProvider::builder()
+            ->setResource($resource)
+            ->addLogRecordProcessor($logProcessor)
+            ->build();
 
         Sdk::builder()
             ->setTracerProvider($tracerProvider)
+            ->setLoggerProvider($loggerProvider)
             ->setPropagator($propagator)
             ->setAutoShutdown(true)
             ->buildAndRegisterGlobal();
@@ -143,21 +160,53 @@ class LaravelOpenTelemetryServiceProvider extends PackageServiceProvider
         $envRepository->set('OTEL_PHP_DEBUG_SCOPES_DISABLED', '1');
     }
 
-    protected function buildOtlpExporter(): OtlpSpanExporter
+    protected function buildSpanExporter(): SpanExporterInterface
     {
-        $transport = match (config('opentelemetry.exporters.otlp.protocol')) {
-            'grpc' => (new GrpcTransportFactory())->create(config('opentelemetry.exporters.otlp.endpoint').OtlpUtil::method(Signals::TRACE)),
+        $tracesExporter = config('opentelemetry.traces.exporter');
+        $tracesExporterConfig = config(sprintf("opentelemetry.exporters.%s", $tracesExporter));
+        $tracesExporterDriver = is_array($tracesExporterConfig) ? $tracesExporterConfig['driver'] : $tracesExporter;
+
+        return match ($tracesExporterDriver) {
+            'zipkin' => new ZipkinExporter(
+                PsrTransportFactory::discover()->create(
+                    Str::of(Arr::get($tracesExporterConfig ?? [], 'endpoint'))->rtrim('/')->append('/api/v2/spans')->toString(),
+                    'application/json'
+                ),
+            ),
+            'otlp' => new OtlpSpanExporter($this->buildOtlpTransport($tracesExporterConfig ?? [])),
+            'console' => (new ConsoleSpanExporterFactory())->create(),
+            default => (new InMemorySpanExporterFactory())->create(),
+        };
+    }
+
+    protected function buildLogsExporter(): LogRecordExporterInterface
+    {
+        $logsExporter = config('opentelemetry.logs.exporter');
+        $logsExporterConfig = config(sprintf("opentelemetry.exporters.%s", $logsExporter));
+        $logsExporterDriver = is_array($logsExporterConfig) ? $logsExporterConfig['driver'] : $logsExporter;
+
+        return match ($logsExporterDriver) {
+            'otlp' => new LogsExporter($this->buildOtlpTransport($logsExporterConfig ?? [])),
+            'console' => (new LogsConsoleExporterFactory())->create(),
+            default => (new LogsInMemoryExporterFactory())->create()
+        };
+    }
+
+    protected function buildOtlpTransport(array $config): TransportInterface
+    {
+        $protocol = $config['protocol'] ?? null;
+        $endpoint = $config['endpoint'] ?? 'http://localhost:4318';
+
+        return match ($protocol) {
+            'grpc' => (new GrpcTransportFactory())->create($endpoint.OtlpUtil::method(Signals::TRACE)),
             'http/json', 'json' => (new OtlpHttpTransportFactory())->create(
-                (new HttpEndpointResolver())->resolveToString(config('opentelemetry.exporters.otlp.endpoint'), Signals::TRACE),
+                (new HttpEndpointResolver())->resolveToString($endpoint, Signals::TRACE),
                 'application/json'
             ),
             default => (new OtlpHttpTransportFactory())->create(
-                (new HttpEndpointResolver())->resolveToString(config('opentelemetry.exporters.otlp.endpoint'), Signals::TRACE),
+                (new HttpEndpointResolver())->resolveToString($endpoint, Signals::TRACE),
                 'application/x-protobuf'
             ),
         };
-
-        // @phpstan-ignore-next-line
-        return new OtlpSpanExporter($transport);
     }
 }
