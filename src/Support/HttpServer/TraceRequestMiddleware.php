bb59c8c17e0c9616023ed57805bf788fbdcdef14
Andrew Broberg <andrew@broberg.id.au>
2025-07-14 11:31:51 +1000

    fix: correct spelling for app bootstrap span

--- src/Support/HttpServer/TraceRequestMiddleware.php
+++ src/Support/HttpServer/TraceRequestMiddleware.php
@@ -29,7 +29,7 @@ class TraceRequestMiddleware
         Tracer::updateLogContext();
 
         if ($bootedTimestamp) {
-            Tracer::newSpan('app boostrap')
+            Tracer::newSpan('app bootstrap')
                 ->setStartTimestamp($bootedTimestamp)
                 ->start()
                 ->end();
