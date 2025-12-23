ac97b53940639846779b4278412ef0c78b43ba07
Fabio Capucci <fabio.cappuc@gmail.com>
2025-12-23 18:42:18 +0100

    skip test on older laravel versions

--- tests/Instrumentation/HttpServerInstrumentationTest.php
+++ tests/Instrumentation/HttpServerInstrumentationTest.php
@@ -466,4 +466,7 @@ it('record forwarded ip from trusted proxies', function () {
         ->toMatchArray([
             \OpenTelemetry\SemConv\Attributes\NetworkAttributes::NETWORK_PEER_ADDRESS => '93.125.25.10',
         ]);
-});
+})->skip(
+    fn () => ! method_exists(\Illuminate\Http\Middleware\TrustProxies::class, 'at'),
+    'Requires laravel 11.31+'
+);
