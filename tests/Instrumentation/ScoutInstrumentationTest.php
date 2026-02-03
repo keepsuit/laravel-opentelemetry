19b2263bc04f938dec4107f3917f546caba8565c
Fabio Capucci <f.capucci@keepsuit.com>
2026-02-03 10:35:28 +0100

    laravel scout instrumentation (#75)

---
+++ tests/Instrumentation/ScoutInstrumentationTest.php
@@ -0,0 +1,145 @@
+<?php
+
+use Keepsuit\LaravelOpenTelemetry\Instrumentation\ScoutInstrumentation;
+use Keepsuit\LaravelOpenTelemetry\Tests\Support\SearchableProduct;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\SDK\Trace\SpanDataInterface;
+use OpenTelemetry\SemConv\Attributes\DbAttributes;
+use OpenTelemetry\SemConv\Metrics\DbMetrics;
+
+test('trace scout search operation', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    SearchableProduct::query()->create(['name' => 'Lorem ipsum']);
+
+    withRootSpan(function () {
+        return SearchableProduct::search('lorem')->get();
+    });
+
+    $spans = getRecordedSpans();
+
+    $searchSpan = $spans->first(fn (SpanDataInterface $span) => $span->getName() === 'search products');
+
+    expect($searchSpan)
+        ->getName()->toBe('search products')
+        ->getKind()->toBe(SpanKind::KIND_CLIENT)
+        ->getAttributes()
+        ->toMatchArray([
+            'db.system.name' => 'scout_collection',
+            'db.namespace' => 'products',
+            'db.operation.name' => 'search',
+            'db.query.text' => 'lorem',
+        ]);
+});
+
+test('trace scout paginate operation', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    SearchableProduct::query()->create(['name' => 'Lorem ipsum']);
+
+    withRootSpan(function () {
+        return SearchableProduct::search('lorem')->paginate();
+    });
+
+    $spans = getRecordedSpans();
+
+    $searchSpan = $spans->first(fn (SpanDataInterface $span) => $span->getName() === 'search products');
+
+    expect($searchSpan)
+        ->getName()->toBe('search products')
+        ->getKind()->toBe(SpanKind::KIND_CLIENT)
+        ->getAttributes()
+        ->toMatchArray([
+            'db.system.name' => 'scout_collection',
+            'db.namespace' => 'products',
+            'db.operation.name' => 'search',
+            'db.query.text' => 'lorem (page: 1, per_page: 15)',
+        ]);
+});
+
+test('trace scout update operation', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    $product = SearchableProduct::query()->create(['name' => 'Lorem ipsum']);
+
+    withRootSpan(function () use ($product) {
+        $product->searchableSync();
+    });
+
+    $spans = getRecordedSpans();
+
+    $searchSpan = $spans->first(fn (SpanDataInterface $span) => $span->getName() === 'search_update products');
+
+    expect($searchSpan)
+        ->getName()->toBe('search_update products')
+        ->getKind()->toBe(SpanKind::KIND_CLIENT)
+        ->getAttributes()
+        ->toMatchArray([
+            'db.system.name' => 'scout_collection',
+            'db.namespace' => 'products',
+            'db.operation.name' => 'search_update',
+            'db.operation.batch.size' => 1,
+            'db.operation.batch.ids' => '1',
+        ]);
+});
+
+test('trace scout delete operation', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    $product = SearchableProduct::query()->create(['name' => 'Lorem ipsum']);
+
+    withRootSpan(function () use ($product) {
+        $product->delete();
+    });
+
+    $spans = getRecordedSpans();
+
+    $searchSpan = $spans->first(fn (SpanDataInterface $span) => $span->getName() === 'search_delete products');
+
+    expect($searchSpan)
+        ->getName()->toBe('search_delete products')
+        ->getKind()->toBe(SpanKind::KIND_CLIENT)
+        ->getAttributes()
+        ->toMatchArray([
+            'db.system.name' => 'scout_collection',
+            'db.namespace' => 'products',
+            'db.operation.name' => 'search_delete',
+            'db.operation.batch.size' => 1,
+            'db.operation.batch.ids' => '1',
+        ]);
+});
+
+test('scout instrumentation skips spans when trace not started', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    SearchableProduct::search('lorem')->first();
+
+    expect(getRecordedSpans())->toBeEmpty();
+});
+
+test('records scout operation duration metric', function () {
+    registerInstrumentation(ScoutInstrumentation::class);
+
+    SearchableProduct::query()->create(['name' => 'Lorem ipsum']);
+
+    withRootSpan(function () {
+        return SearchableProduct::search('lorem')->get();
+    });
+
+    $metric = getRecordedMetrics()->where('name', DbMetrics::DB_CLIENT_OPERATION_DURATION)->sole();
+
+    expect($metric)
+        ->toBeInstanceOf(\OpenTelemetry\SDK\Metrics\Data\Metric::class)
+        ->unit->toBe('s')
+        ->data->toBeInstanceOf(\OpenTelemetry\SDK\Metrics\Data\Histogram::class);
+
+    /** @var \OpenTelemetry\SDK\Metrics\Data\HistogramDataPoint $dataPoint */
+    $dataPoint = $metric->data->dataPoints[0];
+
+    expect($dataPoint->attributes)
+        ->toMatchArray([
+            DbAttributes::DB_SYSTEM_NAME => 'scout_collection',
+            DbAttributes::DB_NAMESPACE => 'products',
+            DbAttributes::DB_OPERATION_NAME => 'search',
+        ]);
+});
